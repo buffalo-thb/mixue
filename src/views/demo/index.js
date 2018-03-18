2a26aba5c8ce4841f86350eae9c1ae5925384e54
tanghaibo <tanghaibo@58ganji.com>
2018-03-18 22:47:59 +0800

    添加router

--- src/views/demo/index.js
+++ src/views/demo/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import VueExt from '@/plugins/vue.ext';
 import IndexView from './index.vue';
 
+Vue.use(VueRouter);
 Vue.use(VueExt);
 
 new Vue({
